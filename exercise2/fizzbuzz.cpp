691dabe34f2873376280054cbb04acfe39279069
Raymond Ortiz <rortiz9@gatech.edu>
2016-09-26 14:26:25 -0400

    Added exercise2 with starter code

---
+++ exercise2/fizzbuzz.cpp
@@ -0,0 +1,32 @@
+// The FizzBuzz problem is as follows:
+//
+// 1. If the integer is divisible by 3, print "Fizz" sans quotes
+// 2. If the integer is divisible by 5, print "Buzz" sans quotes
+// 3. If the integer is divisible by both 3 and 5, print "FizzBuzz" sans quotes
+// 4. If all else fails, print the integer
+//
+// Do this for the first 100 integers (1-100).
+//
+// For example, the first 15 integers:
+//
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+
+#include <iostream>
+
+int main() {
+  // your code here
+}
